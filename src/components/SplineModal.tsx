75c01a1a75f3a3607b4af6ffd27c93bc11576864
hector3211e <hectororopesa5@gmail.com>
2023-06-21 23:26:24 -0400

    changed: spline rocket into splinemodal

--- src/components/SplineModal.tsx
+++ src/components/SplineModal.tsx
@@ -4,14 +4,15 @@ import { Suspense } from "react";
 export default function Rocket() {
   function onLoad(splineApp: Application) {
     // save the app in a ref for later use
-    splineApp.setZoom(1.9);
+    splineApp.setZoom(1);
   }
+
   return (
-    <div className="h-60 px-2 md:h-72 md:px-0">
-      <Suspense fallback={<div>Loadding...</div>}>
+    <div className="h-60 px-2 md:h-80 md:px-0">
+      <Suspense fallback={<p>Loadding...</p>}>
         <Spline
           style={{ borderRadius: 10, boxShadow: "0px 0px 20px 1px black" }}
-          scene="https://prod.spline.design/mfK65j4zF0rnj1tW/scene.splinecode"
+          scene="https://prod.spline.design/kAJ4mFZRWfP2LZuM/scene.splinecode"
           onLoad={onLoad}
         />
       </Suspense>
