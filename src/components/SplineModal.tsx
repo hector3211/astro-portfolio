dc1f8a5629ae677d2b361deae4b7c01bf52519ba
hector3211e <hectororopesa5@gmail.com>
2023-10-15 17:45:47 -0400

    changed: project page and hero section

--- src/components/SplineModal.tsx
+++ src/components/SplineModal.tsx
@@ -1,6 +1,13 @@
 import Spline from "@splinetool/react-spline";
 import type { Application } from "@splinetool/runtime";
 import { Suspense } from "react";
+
+function Spinner() {
+  return (
+    <div className="w-5 h-5 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+  );
+}
+
 export default function Rocket() {
   function onLoad(splineApp: Application) {
     // save the app in a ref for later use
@@ -8,14 +15,14 @@ export default function Rocket() {
   }
 
   return (
-    <div className="h-60 px-2 md:h-80 md:px-0">
-      <Suspense fallback={<p>Loadding...</p>}>
+    <Suspense fallback={<Spinner />}>
+      <div className="h-60 px-2 md:h-80 md:px-0">
         <Spline
           style={{ borderRadius: 10, boxShadow: "0px 0px 20px 2px black" }}
           scene="https://prod.spline.design/kAJ4mFZRWfP2LZuM/scene.splinecode"
           onLoad={onLoad}
         />
-      </Suspense>
-    </div>
+      </div>
+    </Suspense>
   );
 }
