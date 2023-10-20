d4c7af3228a4a75ecbeb7495386847ce64475214
hector3211e <hectororopesa5@gmail.com>
2023-10-19 23:18:57 -0400

    changed: project page new buttons and skills section

--- src/components/SplineModal.tsx
+++ src/components/SplineModal.tsx
@@ -3,26 +3,26 @@ import type { Application } from "@splinetool/runtime";
 import { Suspense } from "react";
 
 function Spinner() {
-  return (
-    <div className="w-5 h-5 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
-  );
+    return (
+        <div className="w-5 h-5 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+    );
 }
 
 export default function Rocket() {
-  function onLoad(splineApp: Application) {
-    // save the app in a ref for later use
-    splineApp.setZoom(1);
-  }
+    function onLoad(splineApp: Application) {
+        // save the app in a ref for later use
+        splineApp.setZoom(1);
+    }
 
-  return (
-    <div className="h-60 pt-5 px-2 md:h-80 md:px-0">
-      <Suspense fallback={<Spinner />}>
-        <Spline
-          style={{ borderRadius: 10, boxShadow: "0px 0px 20px 2px black" }}
-          scene="https://prod.spline.design/kAJ4mFZRWfP2LZuM/scene.splinecode"
-          onLoad={onLoad}
-        />
-      </Suspense>
-    </div>
-  );
+    return (
+        <div className="h-60 py-5 px-2 md:h-80 md:px-0">
+            <Suspense fallback={<Spinner />}>
+                <Spline
+                    style={{ borderRadius: 10, boxShadow: "0px 0px 20px 2px black" }}
+                    scene="https://prod.spline.design/kAJ4mFZRWfP2LZuM/scene.splinecode"
+                    onLoad={onLoad}
+                />
+            </Suspense>
+        </div>
+    );
 }
