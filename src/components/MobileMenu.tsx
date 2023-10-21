67cd9854ef9712b8b30d7c04cb6fbdd4580d051f
hector3211e <hectororopesa5@gmail.com>
2023-10-20 22:55:59 -0400

    changed: hero section layout again

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -2,45 +2,45 @@ import { useEffect, useState } from "react";
 import { TbMenu2 } from "react-icons/tb/index.js";
 
 export default function Menu() {
-  const [show, setShow] = useState<true | false>(true);
+    const [show, setShow] = useState<true | false>(true);
 
-  useEffect(() => {
-    setShow(false);
-  }, []);
+    useEffect(() => {
+        setShow(false);
+    }, []);
 
-  function handleShow() {
-    setShow(!show);
-  }
+    function handleShow() {
+        setShow(!show);
+    }
 
-  return (
-    <div className="relative z-50 md:hidden">
-      <button
-        className="btn bg-gray-600 hover:bg-gray-700 active:bg-gray-700"
-        onClick={handleShow}
-      >
-        <TbMenu2 className="text-lg" />
-      </button>
-      {show && (
-        <div
-          className={`absolute border border-gray-300 top-14 right-1 w-32 bg-base-100 bg-opacity-90 backdrop-blur-lg rounded-md text-lg drop-shadow-2xl`}
-        >
-          <ul className="flex flex-col justify-center items-start p-2">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <div className="divider p-0 m-0 text-black"></div>
-            <li>
-              <a href="/projects">Projects</a>
-            </li>
-            <div className="divider p-0 m-0 text-black"></div>
-            <li>
-              <a href="https://github.com/hector3211" target="_blank">
-                Github
-              </a>
-            </li>
-          </ul>
+    return (
+        <div className="relative z-50 md:hidden">
+            <button
+                className="btn bg-gray-600 hover:bg-gray-700 active:bg-gray-700"
+                onClick={handleShow}
+            >
+                <TbMenu2 className="text-lg" />
+            </button>
+            {show && (
+                <div
+                    className={`absolute border border-gray-300 top-14 right-1 w-32 bg-base-100 bg-opacity-90 backdrop-blur-lg rounded-md text-lg drop-shadow-2xl`}
+                >
+                    <ul className="flex flex-col justify-center items-start p-2">
+                        <li>
+                            <a href="/" onClick={handleShow}>Home</a>
+                        </li>
+                        <div className="divider p-0 m-0 text-black"></div>
+                        <li>
+                            <a href="/projects" onClick={handleShow}>Projects</a>
+                        </li>
+                        <div className="divider p-0 m-0 text-black"></div>
+                        <li>
+                            <a href="https://github.com/hector3211" target="_blank">
+                                Github
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -2,38 +2,37 @@ import { useEffect, useState } from "react";
 import { BsMoonFill, BsSunFill } from "react-icons/bs/index.js";
 
 export default function Toggle() {
-  const [current, setCurrent] = useState<string>("");
+    const [current, setCurrent] = useState<string>("");
 
-  useEffect(() => {
-    if (localStorage.getItem("theme") === "dark") {
-      setCurrent("dark");
-    } else {
-      setCurrent("light");
+    useEffect(() => {
+        if (localStorage.getItem("theme") === "dark") {
+            setCurrent("dark");
+        } else {
+            setCurrent("light");
+        }
+    }, []);
+
+    function changeTheme() {
+        if (localStorage.getItem("theme") === "dark") {
+            setCurrent("dark");
+        } else {
+            setCurrent("light");
+        }
     }
-  }, []);
 
-  function changeTheme() {
-    if (localStorage.getItem("theme") === "dark") {
-      setCurrent("dark");
-    } else {
-      setCurrent("light");
-    }
-  }
-
-  return (
-    <div className="pr-1">
-      <button
-        className={`text-lg text-gray-50 ${
-          current === "light"
-            ? "btn btn-primary"
-            : "btn bg-orange-500 hover:bg-orange-600"
-        }`}
-        data-toggle-theme="dark,light"
-        data-act-class="ACTIVECLASS"
-        onClick={changeTheme}
-      >
-        {current === "light" ? <BsMoonFill /> : <BsSunFill />}
-      </button>
-    </div>
-  );
+    return (
+        <div className="pr-1">
+            <button
+                className={`text-lg text-gray-50 ${current === "light"
+                    ? "btn btn-primary"
+                    : "btn bg-orange-500 hover:bg-orange-600"
+                    }`}
+                data-toggle-theme="dark,light"
+                data-act-class="ACTIVECLASS"
+                onClick={changeTheme}
+            >
+                {current === "light" ? <BsMoonFill className="" /> : <BsSunFill className="" />}
+            </button>
+        </div>
+    );
 }
