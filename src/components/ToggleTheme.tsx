a82eec3044fdb9fd4a82e12981fbfae1666a019f
hector3211e <hectororopesa5@gmail.com>
2024-11-02 19:40:51 -0400

    update: granitesync

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -3,37 +3,37 @@ import { BsMoonFill, BsSunFill } from "react-icons/bs/index.js";
 import { Button } from "../components/ui/button.tsx";
 
 export default function Toggle() {
-    const [current, setCurrent] = useState<string>("");
+  const [current, setCurrent] = useState<string>("");
 
-    useEffect(() => {
-        if (localStorage.getItem("theme") === "dark") {
-            setCurrent("dark");
-        } else {
-            setCurrent("light");
-        }
-    }, []);
-
-    function changeTheme() {
-        const element = document.documentElement;
-        element.classList.toggle("dark");
-
-        const isDark = element.classList.contains("dark");
-        localStorage.setItem("theme", isDark ? "dark" : "light");
-        if (localStorage.getItem("theme") === "dark") {
-            setCurrent("dark");
-        } else {
-            setCurrent("light");
-        }
+  useEffect(() => {
+    if (localStorage.getItem("theme") === "dark") {
+      setCurrent("dark");
+    } else {
+      setCurrent("light");
     }
-    return (
-        <div className="pr-2">
-            <Button className={`text-lg `} onClick={changeTheme}>
-                {current === "light" ? (
-                    <BsMoonFill className="" />
-                ) : (
-                    <BsSunFill className="" />
-                )}
-            </Button>
-        </div>
-    );
+  }, []);
+
+  function changeTheme() {
+    const element = document.documentElement;
+    element.classList.toggle("dark");
+
+    const isDark = element.classList.contains("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+    if (localStorage.getItem("theme") === "dark") {
+      setCurrent("dark");
+    } else {
+      setCurrent("light");
+    }
+  }
+  return (
+    <div className="pr-2">
+      <Button className={`text-lg `} onClick={changeTheme}>
+        {current === "light" ? (
+          <BsMoonFill className="" />
+        ) : (
+          <BsSunFill className="" />
+        )}
+      </Button>
+    </div>
+  );
 }
