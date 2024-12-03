0668c85a853dfa5d7d57c42ac5d2962ccf347178
hector3211e <hectororopesa5@gmail.com>
2024-12-02 20:00:26 -0500

    update

--- src/components/SplineModal.tsx
+++ src/components/SplineModal.tsx
@@ -1,40 +1,30 @@
 import type { Application } from "@splinetool/runtime";
-import React, { useEffect, useRef, useState } from "react";
-const Spline = React.lazy(() => import("@splinetool/react-spline"));
+import { useCallback, useRef, Suspense } from "react";
+import Spline from "@splinetool/react-spline";
 
-function Spinner() {
-  return (
-    <div className="absolute top-1/2 left-[45%] sm:left-1/2 w-10 h-10 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
-  );
-}
-
-export default function Rocket() {
+export default function Model() {
   let modal = useRef<Application | undefined>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const loadTimer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    return () => clearTimeout(loadTimer);
-  }, []);
-  function onLoad(splineApp: Application) {
+  const onLoad = useCallback((splineApp: Application) => {
     modal.current = splineApp;
     // splineApp.setZoom(1);
     // splineApp.setBackgroundColor("black");
-  }
+  }, []);
 
   return (
-    <div className="relative w-full h-80 py-5 md:h-96 md:px-0">
-      {loading ? (
-        <Spinner />
-      ) : (
+    <div className="relative h-80 w-full py-5 md:h-96 md:px-0">
+      <Suspense
+        fallback={
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="h-12 w-12 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
+          </div>
+        }
+      >
         <Spline
           style={{ borderRadius: "4px", width: "100%" }}
           scene="https://prod.spline.design/jNzC38qWlP4eDXCX/scene.splinecode"
           onLoad={onLoad}
         />
-      )}
+      </Suspense>
     </div>
   );
 }
