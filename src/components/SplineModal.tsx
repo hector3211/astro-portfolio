9c422edb0044eb01a3e527224bb80c1e1bd52367
hector3211 <hectororopesa5@gmail.com>
2026-04-11 13:02:58 -0400

    update: spline quality

--- src/components/SplineModal.tsx
+++ src/components/SplineModal.tsx
@@ -6,12 +6,12 @@ export default function Model() {
   let modal = useRef<Application | undefined>();
   const onLoad = useCallback((splineApp: Application) => {
     modal.current = splineApp;
-    // splineApp.setZoom(1);
+    splineApp.setZoom(1.1);
     // splineApp.setBackgroundColor("black");
   }, []);
 
   return (
-    <div className="relative h-80 w-full py-5 md:h-96 md:px-0">
+    <div className="relative h-[24rem] w-full overflow-hidden rounded-md py-5 md:h-[30rem] md:px-0 lg:h-[34rem]">
       <Suspense
         fallback={
           <div className="absolute inset-0 flex items-center justify-center">
@@ -20,7 +20,9 @@ export default function Model() {
         }
       >
         <Spline
-          style={{ borderRadius: "4px", width: "100%" }}
+          renderOnDemand={false}
+          className="h-full w-full"
+          style={{ borderRadius: "6px", width: "100%", height: "100%" }}
           scene="https://prod.spline.design/jNzC38qWlP4eDXCX/scene.splinecode"
           onLoad={onLoad}
         />
